ab5e61dc505c2267eeb4c8ff699936c74ba0eaba
0x89A <57545695+0x89A@users.noreply.github.com>
2020-09-06 13:52:24 +0100

    Fixed NRE and optimised code

--- NeutralAnimals.cs
+++ NeutralAnimals.cs
@@ -4,7 +4,8 @@ using Newtonsoft.Json;
 
 namespace Oxide.Plugins
 {
-    [Info("Neutral Animals", "0x89A", "1.0.7")]
+    [Info("Neutral Animals", "0x89A", "1.0.8")]
+    [Description("Animals only attack if they are attacked first")]
     class NeutralAnimals : RustPlugin
     {
         private const string canuse = "neutralanimals.use";
@@ -39,9 +40,9 @@ namespace Oxide.Plugins
             public float forgetTime = 30f;
         }
 
-        object OnNpcTarget(BaseAnimalNPC animal, BaseEntity entity)
+        object OnNpcTarget(BaseAnimalNPC animal, BasePlayer target)
         {
-            if (entity.IsNpc || animal.lastAttacker == null || !permission.UserHasPermission(entity.net.connection.userid.ToString(), canuse) || (animal.lastAttacker == entity && (Time.time - animal.lastAttackedTime) < _config.forgetTime)) return null;
+            if (target.IsNpc || !permission.UserHasPermission(target.UserIDString, canuse) || (animal.lastAttacker == target && (Time.time - animal.lastAttackedTime) < _config.forgetTime)) return null;
             else return true;
         }
     }
